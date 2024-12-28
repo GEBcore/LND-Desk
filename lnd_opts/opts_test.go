c2466d7036b464043caa840841956954e6247659
Gkirito <lownkid@outlook.com>
2024-12-28 11:56:03 +0800

    ✅ (opts_test.go): add test for ListAddresses function to ensure it works correctly

--- lnd_opts/opts_test.go
+++ lnd_opts/opts_test.go
@@ -114,3 +114,40 @@ func TestGetState(t *testing.T) {
 		})
 	}
 }
+
+func TestListAddresses(t *testing.T) {
+	interceptor, err := signal.Intercept()
+	if err != nil {
+		t.Fatal(err)
+	}
+	lndConfig, err := lnd_node.CheckAndParse(interceptor, "", bytes.NewBufferString("[Application Options]\ndebuglevel=trace\nmaxpendingchannels=10\nalias=Bevm_client_test\nno-macaroons=false\ncoin-selection-strategy=largest\nrpclisten=localhost:10009\nrestlisten=localhost:8080\nno-rest-tls=true\nrestcors=https://bevmhub.bevm.io\n\n[Bitcoin]\nbitcoin.mainnet=false\nbitcoin.testnet=false\nbitcoin.simnet=false\nbitcoin.regtest=false\nbitcoin.signet=true\nbitcoin.node=neutrino\n\n[neutrino]\nneutrino.addpeer=x49.seed.signet.bitcoin.sprovoost.nl\nneutrino.addpeer=v7ajjeirttkbnt32wpy3c6w3emwnfr3fkla7hpxcfokr3ysd3kqtzmqd.onion:38333\n\n[protocol]\nprotocol.simple-taproot-chans=true"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	type args struct {
+		c *lnd.Config
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "test",
+			args: args{
+				c: lndConfig,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ListAddresses(tt.args.c)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ListAddresses() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			t.Log(got)
+		})
+	}
+}
