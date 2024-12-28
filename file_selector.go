e40f5973270d6e50bf67ab75e0f32dee58ee440a
Gkirito <lownkid@outlook.com>
2024-12-28 12:23:04 +0800

    🔧 (file_selector.go): add check for file existence in default directory before opening dialog

--- file_selector.go
+++ file_selector.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"errors"
+	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
 func (a *App) OpenFileSelector(opts runtime.OpenDialogOptions) (string, error) {
+	if !lnrpc.FileExists(opts.DefaultDirectory) {
+		opts.DefaultDirectory = ""
+	}
 	dir, err := runtime.OpenDirectoryDialog(a.ctx, opts)
 	if err != nil {
 		return "", err
