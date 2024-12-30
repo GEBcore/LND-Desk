bf4a84907728b2141dc8d125d6b4046bec4ac070
Gkirito <lownkid@outlook.com>
2024-12-30 16:42:50 +0800

    📝 (index.html, main.go, wails.json): update title from lowercase to uppercase for consistency and readability

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "lnd-desk",
+		Title:  "LND Desk",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
