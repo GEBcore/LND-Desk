94f4fbe58d0f9cc0b03d410c364715a551775429
Gkirito <lownkid@outlook.com>
2025-02-11 09:59:34 +0800

    📝 (lndChainInfo.tsx): update lnd chain scan map and add missing network options to frameworks list collection

--- frontend/src/commons/lndChainInfo.tsx
+++ frontend/src/commons/lndChainInfo.tsx
@@ -2,15 +2,15 @@ import { createListCollection } from '@chakra-ui/react';
 
 export const frameworks = createListCollection({
   items: [
-    // { label: "mainnet", value: "mainnet" },
-    // { label: "testnet", value: "testnet" },
-    // { label: "simnet", value: "simnet" },
-    // { label: "regtest", value: "regtest" },
+    { label: "mainnet", value: "mainnet" },
+    { label: "testnet", value: "testnet" },
+    { label: "simnet", value: "simnet" },
+    { label: "regtest", value: "regtest" },
     { label: "signet", value: "signet" },
   ],
 })
 
 export const lndChainScanMap: { [key: string]: string } = {
-  'mainnet':'https://mempool.space/',
+  'mainnet':'https://mempool.space',
   'signet':'https://mempool.space/signet'
 }
