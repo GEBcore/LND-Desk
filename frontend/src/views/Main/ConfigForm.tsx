410a2e58503361cd7d88ca6bdacd4dcaf1be8bcf
xl <1293358059@qq.com>
2025-02-11 13:41:33 +0800

    fix: configform

--- frontend/src/views/Main/ConfigForm.tsx
+++ frontend/src/views/Main/ConfigForm.tsx
@@ -147,12 +147,6 @@ const ConfigForm = () => {
                    } />
           </div>
         </div>
-        <h2>Updated Config</h2>
-        <textarea
-          style={{ width: '100%', height: '200px' }}
-          value={config}
-          readOnly
-        />
     </div>
     </>
   );
